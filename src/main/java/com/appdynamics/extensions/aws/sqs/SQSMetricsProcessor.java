d342714e1010f91387d7398b88cc14a813dcfd3d
satish.muddam <satish.muddam@appdynamics.com>
2017-05-18 15:41:49 +0530

    Code changes to add instance profile support.

--- src/main/java/com/appdynamics/extensions/aws/sqs/SQSMetricsProcessor.java
+++ src/main/java/com/appdynamics/extensions/aws/sqs/SQSMetricsProcessor.java
@@ -33,7 +33,7 @@ public class SQSMetricsProcessor implements MetricsProcessor {
         this.excludeMetricsPattern = MetricsProcessorHelper.createPattern(excludeMetrics);
     }
 
-    public List<Metric> getMetrics(AmazonCloudWatch awsCloudWatch) {
+    public List<Metric> getMetrics(AmazonCloudWatch awsCloudWatch, String accountName) {
         return MetricsProcessorHelper.getFilteredMetrics(awsCloudWatch,
                 NAMESPACE,
                 excludeMetricsPattern,
