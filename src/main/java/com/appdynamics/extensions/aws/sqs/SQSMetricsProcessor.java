cbc0d3db5d3ade07fb2b55feb7d45790f2681597
akshayAppd <akshay.srivastava@appdynamics.com>
2020-05-14 14:41:50 +0530

    Upgrade to aws commons 2.2.3.1.

--- src/main/java/com/appdynamics/extensions/aws/sqs/SQSMetricsProcessor.java
+++ src/main/java/com/appdynamics/extensions/aws/sqs/SQSMetricsProcessor.java
@@ -9,18 +9,22 @@
 package com.appdynamics.extensions.aws.sqs;
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
-import com.amazonaws.services.cloudwatch.model.Metric;
-import com.appdynamics.extensions.aws.config.MetricType;
+import com.amazonaws.services.cloudwatch.model.DimensionFilter;
+import com.appdynamics.extensions.aws.config.Dimension;
+import com.appdynamics.extensions.aws.config.IncludeMetric;
+import com.appdynamics.extensions.aws.dto.AWSMetric;
 import com.appdynamics.extensions.aws.metric.NamespaceMetricStatistics;
 import com.appdynamics.extensions.aws.metric.StatisticType;
 import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
 import com.appdynamics.extensions.aws.metric.processors.MetricsProcessorHelper;
+import com.appdynamics.extensions.aws.predicate.MultiDimensionPredicate;
+import com.appdynamics.extensions.metrics.Metric;
+import com.google.common.collect.Lists;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.regex.Pattern;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * @author Satish Muddam
@@ -29,32 +33,38 @@ public class SQSMetricsProcessor implements MetricsProcessor {
 
     private static final String NAMESPACE = "AWS/SQS";
 
-    private static final String[] DIMENSIONS = {"QueueName"};
+    private List<IncludeMetric> includeMetrics;
 
-    private List<MetricType> metricTypes;
+    private List<Dimension> dimensions;
 
-    private Pattern excludeMetricsPattern;
-
-    public SQSMetricsProcessor(List<MetricType> metricTypes,
-                               Set<String> excludeMetrics) {
-        this.metricTypes = metricTypes;
-        this.excludeMetricsPattern = MetricsProcessorHelper.createPattern(excludeMetrics);
+    public SQSMetricsProcessor(List<IncludeMetric> includeMetrics, List<Dimension> dimensions) {
+        this.includeMetrics = includeMetrics;
+        this.dimensions = dimensions;
     }
 
-    public List<Metric> getMetrics(AmazonCloudWatch awsCloudWatch, String accountName) {
-        return MetricsProcessorHelper.getFilteredMetrics(awsCloudWatch,
+    public List<AWSMetric> getMetrics(AmazonCloudWatch awsCloudWatch, String accountName, LongAdder awsRequestsCounter) {
+        List<DimensionFilter> dimensionFilters = getDimensionFilters();
+
+        MultiDimensionPredicate predicate = new MultiDimensionPredicate(dimensions);
+
+        return MetricsProcessorHelper.getFilteredMetrics(awsCloudWatch, awsRequestsCounter,
                 NAMESPACE,
-                excludeMetricsPattern,
-                DIMENSIONS);
+                includeMetrics,
+                dimensionFilters,
+                predicate);
     }
 
-    public StatisticType getStatisticType(Metric metric) {
-        return MetricsProcessorHelper.getStatisticType(metric, metricTypes);
+    public StatisticType getStatisticType(AWSMetric metric) {
+        return MetricsProcessorHelper.getStatisticType(metric.getIncludeMetric(), includeMetrics);
     }
 
-    public Map<String, Double> createMetricStatsMapForUpload(NamespaceMetricStatistics namespaceMetricStats) {
+    public List<Metric> createMetricStatsMapForUpload(NamespaceMetricStatistics namespaceMetricStats) {
         Map<String, String> dimensionToMetricPathNameDictionary = new HashMap<String, String>();
-        dimensionToMetricPathNameDictionary.put(DIMENSIONS[0], "Queue Name");
+
+        for (Dimension dimension : dimensions) {
+            dimensionToMetricPathNameDictionary.put(dimension.getName(), "Queue Name");
+        }
+
 
         return MetricsProcessorHelper.createMetricStatsMapForUpload(namespaceMetricStats,
                 dimensionToMetricPathNameDictionary, false);
@@ -62,6 +72,17 @@ public class SQSMetricsProcessor implements MetricsProcessor {
 
     public String getNamespace() {
         return NAMESPACE;
+    }
+
+    private List<DimensionFilter> getDimensionFilters() {
+
+        List<DimensionFilter> dimensionFilters = Lists.newArrayList();
+        for (Dimension dimension : dimensions) {
+            DimensionFilter dbDimensionFilter = new DimensionFilter();
+            dbDimensionFilter.withName(dimension.getName());
+            dimensionFilters.add(dbDimensionFilter);
+        }
+        return dimensionFilters;
     }
 
 }
